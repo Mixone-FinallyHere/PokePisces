1b4196670a164b6b82d93f121617f9606846c203
Sadfish the Sad <thegreatestfishinexistence@gmail.com>
2025-03-08 12:16:11 -0500

    high roll hit fix

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -16858,6 +16858,7 @@ const struct BattleMove gBattleMoves[MOVES_COUNT_Z] =
         .priority = 0,
         .split = SPLIT_PHYSICAL,
         .zMoveEffect = Z_EFFECT_NONE,
+        .strikeCount = 2,
     },
 
     [MOVE_SHARPSHOOT] =
