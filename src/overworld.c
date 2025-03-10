d5d471f190967b6c6646c6140834b29b28374d54
Chairry <chairvaria@gmail.com>
2025-03-10 12:00:10 -0400

    music edits

--- src/overworld.c
+++ src/overworld.c
@@ -1182,7 +1182,7 @@ void Overworld_PlaySpecialMapMusic(void)
         else if (GetCurrentMapType() == MAP_TYPE_UNDERWATER)
             music = MUS_UNDERWATER;
         else if (TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_SURFING))
-            music = MUS_SURF;
+            music = MUS_SURF_2;
     }
 
     if (music != GetCurrentMapMusic())
@@ -1207,10 +1207,10 @@ static void TransitionMapMusic(void)
         u16 currentMusic = GetCurrentMapMusic();
         if (newMusic != MUS_ABNORMAL_WEATHER && newMusic != MUS_NONE)
         {
-            if (currentMusic == MUS_UNDERWATER || currentMusic == MUS_SURF)
+            if (currentMusic == MUS_UNDERWATER || currentMusic == MUS_SURF_2)
                 return;
             if (TestPlayerAvatarFlags(PLAYER_AVATAR_FLAG_SURFING))
-                newMusic = MUS_SURF;
+                newMusic = MUS_SURF_2;
         }
         if (newMusic != currentMusic)
         {
@@ -1251,7 +1251,7 @@ void TryFadeOutOldMapMusic(void)
     u16 warpMusic = GetWarpDestinationMusic();
     if (FlagGet(FLAG_DONT_TRANSITION_MUSIC) != TRUE && warpMusic != GetCurrentMapMusic())
     {
-        if (currentMusic == MUS_SURF
+        if (currentMusic == MUS_SURF_2
             && VarGet(VAR_SKY_PILLAR_STATE) == 2
             && gSaveBlock1Ptr->location.mapGroup == MAP_GROUP(SOOTOPOLIS_CITY)
             && gSaveBlock1Ptr->location.mapNum == MAP_NUM(SOOTOPOLIS_CITY)
--- src/pokemon.c
+++ src/pokemon.c
@@ -8443,16 +8443,17 @@ u16 GetBattleBGM(void)
         case TRAINER_CLASS_DOME_ACE:
         case TRAINER_CLASS_STROLLER:
         case TRAINER_CLASS_PRESENTER:
+        case TRAINER_CLASS_MAGMA_ADMIN:
             return MUS_VS_ZINNIA;
         case TRAINER_CLASS_TOPAZ_ACOLYTE:
         case TRAINER_CLASS_GILDED_MONK:
             return MUS_EVER_GRANDE_ROAD;
         case TRAINER_CLASS_AQUA_LEADER:
+            return MUS_VS_AQUA_MAGMA_LEADER;
         case TRAINER_CLASS_MAGMA_LEADER:
-            return MUS_VS_CHAMPION;
+            return MUS_VS_OZONE;
         case TRAINER_CLASS_AQUA_ADMIN:
-        case TRAINER_CLASS_MAGMA_ADMIN:
-            return MUS_VS_CHAMPION;
+            return MUS_VS_AQUA_MAGMA;
         case TRAINER_CLASS_LEADER:
             return MUS_VS_GYM_LEADER_2;
         case TRAINER_CLASS_CHAMPION:
@@ -8466,6 +8467,7 @@ u16 GetBattleBGM(void)
         case TRAINER_CLASS_ELITE_FOUR:
             return MUS_VS_ELITE_FOUR;
         case TRAINER_CLASS_SALON_MAIDEN:
+            return MUS_VS_WALLY;
         case TRAINER_CLASS_PALACE_MAVEN:
         case TRAINER_CLASS_ARENA_TYCOON:
         case TRAINER_CLASS_FACTORY_HEAD:
