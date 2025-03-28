8dc0b11a6bf3a533afab85c13b3cc0a694983d39
Sadfish the Sad <thegreatestfishinexistence@gmail.com>
2025-03-28 17:38:45 -0400

    run away buff

--- src/battle_util.c
+++ src/battle_util.c
@@ -8197,6 +8197,8 @@ bool32 CanBattlerEscape(u32 battler) // no ability check
         return TRUE;
     else if (IS_BATTLER_OF_TYPE(battler, TYPE_GHOST))
         return TRUE;
+    else if (GetBattlerAbility(battler) == ABILITY_RUN_AWAY)
+        return TRUE;
     else if (GetBattlerHoldEffect(battler, TRUE) == HOLD_EFFECT_GLUE_TUBE)
         return FALSE;
     else if (gBattleMons[battler].status2 & (STATUS2_ESCAPE_PREVENTION | STATUS2_WRAPPED))
